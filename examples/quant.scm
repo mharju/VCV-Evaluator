e90973859c2c716959195e4a1e44065dc825289a
Mikko Harju <mikko@taiste.fi>
2018-03-12 20:31:22 +0200

    Changed example code

--- examples/quant.scm
+++ examples/quant.scm
@@ -1,4 +1,10 @@
 ;; A simple semitone quantizer
+;; Toggle different scales by evaluating
+;; (set! *current-scale* *minor-7th-freqs*)
+;; (set! *current-scale* *major-7th-freqs*)
+;; etc.
+;; Or evaluate your own scale and then set it like so.
+
 (define *t* 0.0)
 (define *semitone-volt-step* (/ 1 12))
 (define (freqs scale) (fold
@@ -13,6 +19,12 @@
 (define *minor-scale* '(0 2 1 2 2 1 2 2))
 (define *minor-freqs* (freqs *minor-scale*))
 
+(define *major-7th* '(0 4 3 4))
+(define *major-7th-freqs* (freqs *major-7th*))
+
+(define *minor-7th* '(0 3 4 3))
+(define *minor-7th-freqs* (freqs *minor-7th*))
+
 (define (quant-index f q) (floor (/ f q)))
 (define (quant-to-semitone v)
   (let ((base (floor v)))
@@ -24,5 +36,7 @@
          (index (inexact->exact (quant-index (- v base) (/ 1 len)))))
     (+ base (car (drop scale-freqs (- len index 1))))))
 
+(define *current-scale* *minor-7th-freqs*)
+(define *voltage-offset* 0.0) ;; 1v/oct
 (define (run-dsp delta-t in1 in2 in3 in4 out1 out2 out3 out4)
-  (pointer-f32-set! out1 (quant-to-scale *minor-freqs* in1)))
+  (pointer-f32-set! out1 (quant-to-scale *current-scale* (+ in1 *voltage-offset*))))
